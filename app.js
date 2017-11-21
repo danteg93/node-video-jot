9f3bb8b3e7a32478a566c3a5a24b35cdc4090089
Dante Garcia <dantegarciaram93@gmail.com>
2017-11-21 14:36:49 -0800

    port typo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const flash = require('connect-flash');
 const session = require('express-session');
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 //Load Routes
 const ideas = require('./routes/ideas');
